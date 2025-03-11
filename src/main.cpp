bef1cef31916db553fdb3f1d5c21c26a45491b0c
meriyael <skeptik676@gmail.com>
2025-03-11 11:38:58 +0530

    snake lenght

--- src/main.cpp
+++ src/main.cpp
@@ -18,16 +18,20 @@ int main()
 
     int grid = 50;
 
-    int snake_dirx = 0, snake_diry = 0;
+//     // int total = 1;
+    
+
+
+//     // int snake_dirx = 0, snake_diry = 0;
 
     int pl_x = width / grid;
     int pl_y = height / grid;
 
-    SDL_Rect snake;
-    snake.x = 0;
-    snake.y = 0;
-    snake.w = 50;
-    snake.h = 50;
+//     // SDL_Rect snake[100];
+//     // snake[0].x = 0;
+//     // snake[0].y = 0;
+//     // snake[0].w = 50;
+//     // snake[0].h = 50;
 
     SDL_Rect food;
     food.x = (rand() % pl_x) * grid;
@@ -43,61 +47,87 @@ int main()
             {
                 running = false;
             }
-            if (e.type == SDL_KEYDOWN)
-            {
-                if (e.key.keysym.sym == SDLK_RIGHT)
-                {
-                    snake_dirx += 1;
-                    // snake_diry = 0;
-                }
-                if (e.key.keysym.sym == SDLK_LEFT)
-                {
-                    snake_dirx -= 1;
-                    // snake_diry = 0;
-                }
-                if (e.key.keysym.sym == SDLK_UP)
-                {
-                    // snake_dirx = 0;
-                    snake_diry -= 1;
-                }
-                if (e.key.keysym.sym == SDLK_DOWN)
-                {
-                    // snake_dirx = 0;
-                    snake_diry += 1;
-                }
-            }
+//             if (e.type == SDL_KEYDOWN)
+//             {
+//                 if (e.key.keysym.sym == SDLK_RIGHT)
+//                 {
+//                     snake_dirx += 1;
+                    
+//                     // snake_diry = 0;
+//                 }
+//                 if (e.key.keysym.sym == SDLK_LEFT)
+//                 {
+//                     snake_dirx -= 1;
+//                     // snake_diry = 0;
+//                 }
+//                 if (e.key.keysym.sym == SDLK_UP)
+//                 {
+//                     // snake_dirx = 0;
+//                     snake_diry -= 1;
+//                 }
+//                 if (e.key.keysym.sym == SDLK_DOWN)
+//                 {
+//                     // snake_dirx = 0;
+//                     snake_diry += 1;
+//                 }
+//             }
         }
 
-        snake.x = snake_dirx * grid;
-        snake.y = snake_diry * grid;
+//         // snake[0].x = snake_dirx * grid;
+//         // snake[0].y = snake_diry * grid;
 
-        SDL_Delay(100);
+//         SDL_Delay(100);
 
-        if (snake.x == food.x && snake.y == food.y)
-        {
-            food.x = (rand() % pl_x) * grid;
-            food.y = (rand() % pl_y) * grid;
-        }
+//         void newDir(){
+//             int x,y;
+//             x = snake[0].x + snake_dirx*snake[0].w;
+//             y = snake[0].y + snake_diry*snake[0].h;
+//         }
 
-        // if(snake.x + snake.w > width){
-        //     snake.x = width - snake.w;
-        //     snake_dirx = snake.x/grid;
-        // }
-        // if(snake.x < 0){
-        //     snake.x = 0;
-        // }
-        // if(snake.y < 0){
-        //     snake.y = 0;
-        // }
-        // if(snake.y + snake.h > height){
-        //     snake.y = height - snake.h;
-        // }
+//         if (snake[0].x == food.x && snake[0].y == food.y)
+//         {
+//             food.x = (rand() % pl_x) * grid;
+//             food.y = (rand() % pl_y) * grid;
+//             total++ ;
+            
+//         }
+
+
+
+        
+
+//         // if(snake.x + snake.w > width){
+//         //     snake.x = width - snake.w;
+//         //     snake_dirx = snake.x/grid;
+//         // }
+//         // if(snake.x < 0){
+//         //     snake.x = 0;
+//         // }
+//         // if(snake.y < 0){
+//         //     snake.y = 0;
+//         // }
+//         // if(snake.y + snake.h > height){
+//         //     snake.y = height - snake.h;
+//         // }
 
         SDL_SetRenderDrawColor(render, 0, 0, 0, 255);
         SDL_RenderClear(render);
 
-        SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
-        SDL_RenderFillRect(render, &snake);
+
+
+        
+
+
+//         snake[0].x = snake_dirx * grid;
+//         snake[0].y = snake_diry * grid;
+        
+//         for(int i= 0;  i< total; i++){
+
+
+//             SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
+//             SDL_RenderFillRect(render, &snake[i]);
+
+//         }
 
         SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
         SDL_RenderFillRect(render, &food);
